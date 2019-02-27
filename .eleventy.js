7bb94d3b1886c2dd713a1407b2303ec3d9234884
Vadim Makeev <pepelsbey@gmail.com>
2019-02-28 02:20:00 +0300

    Add readableDate filter

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,13 @@
+const { DateTime } = require('luxon');
+
 module.exports = function(config) {
     config.addPassthroughCopy('src/pres');
     config.addPassthroughCopy('src/styles');
     config.addPassthroughCopy('src/fonts');
+    config.addFilter('readableDate', (dateObj) => {
+        return DateTime.fromJSDate(dateObj).setLocale('ru').toFormat('dd MMMM yyyy');
+    });
+
     return {
         dir: {
             input: 'src',
